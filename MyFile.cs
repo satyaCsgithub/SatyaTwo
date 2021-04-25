04e338f8799dfe43f994df4007a4d3d64a4f8cda
satyaCsgithub <satya_inet@yahoo.com>
2021-04-25 22:18:52 +0530

    Add new file called MyFile.cs

---
+++ MyFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class PostDetail
+    {
+        public int PostId { get; set; }
+        public Nullable<int> PostWeight { get; set; }
+        [Display(Name = "Post Name")]
+        public string PostName { get; set; }
+        public Nullable<int> catId { get; set; }
+        [NotMapped]
+        [Display(Name = "Categoty Name")]
+        public string catName { get; set; }
+        public DateTime fromdt { get; set; }
+        public List<PostDetail> usersinfo { get; set; }
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+    }
+}
